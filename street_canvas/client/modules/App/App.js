5a3c57928fbc399ea76548fac8ba03bd368aa805
Brent <karmapoint@gmail.com>
2017-04-03 21:40:09 -0700

    design and style header

--- street_canvas/client/modules/App/App.js
+++ street_canvas/client/modules/App/App.js
@@ -34,7 +34,7 @@ export class App extends Component {
         {this.state.isMounted && !window.devToolsExtension && process.env.NODE_ENV === 'development' && <DevTools />}
         <div>
           <Helmet
-            title="MERN Starter - Blog App"
+            title="StreetCANVAS: Find Street Art Near You!"
             titleTemplate="%s - Blog App"
             meta={[
               { charset: 'utf-8' },
@@ -48,11 +48,7 @@ export class App extends Component {
               },
             ]}
           />
-          <Header
-            switchLanguage={lang => this.props.dispatch(switchLanguage(lang))}
-            intl={this.props.intl}
-            toggleAddPost={this.toggleAddPostSection}
-          />
+          <Header toggleAddPost={this.toggleAddPostSection} />
           <div className={styles.container}>
             {this.props.children}
           </div>
--- street_canvas/client/modules/App/components/Header/Header.js
+++ street_canvas/client/modules/App/components/Header/Header.js
@@ -6,21 +6,15 @@ import { FormattedMessage } from 'react-intl';
 import styles from './Header.css';
 
 export function Header(props, context) {
-  const languageNodes = props.intl.enabledLanguages.map(
-    lang => <li key={lang} onClick={() => props.switchLanguage(lang)} className={lang === props.intl.locale ? styles.selected : ''}>{lang}</li>
-  );
+  // const languageNodes = props.intl.enabledLanguages.map(
+  //   lang => <li key={lang} onClick={() => props.switchLanguage(lang)} className={lang === props.intl.locale ? styles.selected : ''}>{lang}</li>
+  // );
 
   return (
     <div className={styles.header}>
-      <div className={styles['language-switcher']}>
-        <ul>
-          <li><FormattedMessage id="switchLanguage" /></li>
-          {languageNodes}
-        </ul>
-      </div>
       <div className={styles.content}>
         <h1 className={styles['site-title']}>
-          <Link to="/" ><FormattedMessage id="siteTitle" /></Link>
+          <Link to="/" >STREET<span>CANVAS</span></Link>
         </h1>
         {
           context.router.isActive('/', true)
@@ -38,8 +32,6 @@ Header.contextTypes = {
 
 Header.propTypes = {
   toggleAddPost: PropTypes.func.isRequired,
-  switchLanguage: PropTypes.func.isRequired,
-  intl: PropTypes.object.isRequired,
 };
 
 export default Header;
