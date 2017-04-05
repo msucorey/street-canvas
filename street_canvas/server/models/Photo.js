b6477c0d2c8a46cf3bf2fb246c1c759bfc976a1c
Brent <karmapoint@gmail.com>
2017-04-05 10:33:42 -0700

    add photo schema to model

--- street_canvas/server/models/Photo.js
+++ street_canvas/server/models/Photo.js
@@ -2,6 +2,12 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 const PhotoSchema = new Schema({
+  photo_url: { type: 'String', require: true },
+  user_id: { type: 'Integer', required: true },
+  description: { type: 'String' },
+  lat: { type: 'Number', required: true },
+  lng: { type: 'Number', required: true },
+  dateAdded: { type: 'Date', default: Date.now, required: true },
 
 });
 
