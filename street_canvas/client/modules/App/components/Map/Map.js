3dd871c385180fc968df87ca5cfb13a1c0f031a2
Brent <karmapoint@gmail.com>
2017-04-04 22:02:50 -0700

    remove the Intl component from the project

---
+++ street_canvas/client/modules/App/components/Map/Map.js
@@ -0,0 +1,11 @@
+import React, { PropTypes } from 'react';
+
+function Map(props, context) {
+  return (
+  );
+}
+
+Map.propTypes = {
+};
+
+export default Map;
--- street_canvas/client/modules/Post/components/PostCreateWidget/PostCreateWidget.js
+++ street_canvas/client/modules/Post/components/PostCreateWidget/PostCreateWidget.js
@@ -1,5 +1,5 @@
 import React, { Component, PropTypes } from 'react';
-import { injectIntl, intlShape, FormattedMessage } from 'react-intl';
+
 
 // Import Style
 import styles from './PostCreateWidget.css';
@@ -20,11 +20,11 @@ export class PostCreateWidget extends Component {
     return (
       <div className={cls}>
         <div className={styles['form-content']}>
-          <h2 className={styles['form-title']}><FormattedMessage id="createNewPost" /></h2>
-          <input placeholder={this.props.intl.messages.authorName} className={styles['form-field']} ref="name" />
-          <input placeholder={this.props.intl.messages.postTitle} className={styles['form-field']} ref="title" />
-          <textarea placeholder={this.props.intl.messages.postContent} className={styles['form-field']} ref="content" />
-          <a className={styles['post-submit-button']} href="#" onClick={this.addPost}><FormattedMessage id="submit" /></a>
+          <h2 className={styles['form-title']}>Create new post</h2>
+          <input placeholder="author name" className={styles['form-field']} ref="name" />
+          <input placeholder="post title" className={styles['form-field']} ref="title" />
+          <textarea placeholder="message" className={styles['form-field']} ref="content" />
+          <a className={styles['post-submit-button']} href="#" onClick={this.addPost}>Submit</a>
         </div>
       </div>
     );
@@ -34,7 +34,6 @@ export class PostCreateWidget extends Component {
 PostCreateWidget.propTypes = {
   addPost: PropTypes.func.isRequired,
   showAddPost: PropTypes.bool.isRequired,
-  intl: intlShape.isRequired,
 };
 
-export default injectIntl(PostCreateWidget);
+export default PostCreateWidget;
