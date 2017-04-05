2aa42fc0948bfba7570aa5ad1f1ded97e4859c06
Brent <karmapoint@gmail.com>
2017-04-05 16:00:10 -0700

    add photo actions and routes

--- street_canvas/server/routes/Photo.routes.js
+++ street_canvas/server/routes/Photo.routes.js
@@ -2,6 +2,14 @@ import { Router } from 'express';
 import * as PhotoController from '../controllers/Photo.controller';
 
 const router = new Router();
+// Get all Photos
+router.route('/photos').get(PhotoController.getPosts);
+
+// Get one photo by cuid
+router.route('/photos/:cuid').get(PhotoController.getPost);
+
+// Add a new Photo
+router.route('/photos').post(PhotoController.addPost);
 
 
 export default router;
