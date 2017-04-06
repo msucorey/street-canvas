189d153a503847da369994a5b5e83514eb95046d
Brent <karmapoint@gmail.com>
2017-04-05 22:07:33 -0700

    add 3 more seed photos

--- street_canvas/server/dummyData.js
+++ street_canvas/server/dummyData.js
@@ -9,8 +9,17 @@ export default function () {
     const photo1 = new Photo({
       photo_url: 'http://res.cloudinary.com/streetcanvas/image/upload/v1491414710/IMG_20170214_125605_iytljf.jpg',
       description: 'Anatomical bat by Austrian surgeon/street artist, Nychos', lat: '37.7875099', lng: '-122.397319', cuid: 'cikqgkv4q01ck7453ualdn7fq' });
+    const photo2 = new Photo({
+      photo_url: 'http://res.cloudinary.com/streetcanvas/image/upload/v1491454814/dreams_boukn2.jpg',
+      description: 'You don\'t have dreams, you make them', lat: '37.758', lng: '-122.419', cuid: 'cikqgkv4q01ck7453uald3tgsreh' });
+    const photo3 = new Photo({
+      photo_url: 'http://res.cloudinary.com/streetcanvas/image/upload/v1491454813/morning_stairwell_2_bottom_y19gxa.jpg',
+      description: 'Mosaic stairs', lat: '37.756279', lng: '-122.4756077', cuid: 'cikqgkv4q01ck7453uaadfabe' });
+    const photo4 = new Photo({
+      photo_url: 'http://res.cloudinary.com/streetcanvas/image/upload/v1491454815/fire_steel_xxg1nw.jpg',
+      description: 'Steel and Fire', lat: '37.806', lng: '-122.269', cuid: 'cikqgkv4q01ck7453ualdn7fq' });
 
-    Photo.create([photo1], (error) => {
+    Photo.create([photo4, photo3, photo2, photo1], (error) => {
       if (!error) {
       }
     });
