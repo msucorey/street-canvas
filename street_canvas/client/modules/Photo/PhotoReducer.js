d8512bc95700ebb480f7d1b5cd48b6f88bb23312
Brent <karmapoint@gmail.com>
2017-04-05 20:02:13 -0700

    add photo reducers and fix header height issue

--- street_canvas/client/modules/Photo/PhotoReducer.js
+++ street_canvas/client/modules/Photo/PhotoReducer.js
@@ -1,14 +1,31 @@
 // Import Actions
-import {  } from './PhotoActions';
+import { ADD_PHOTO, ADD_PHOTOS } from './PhotoActions';
 
 // Initial State
-const initialState = {};
+const initialState = { data: [] };
 
 const PhotoReducer = (state = initialState, action) => {
   switch (action.type) {
+    case ADD_PHOTO:
+      return {
+        data: [action.photo, ...state.data],
+      };
+    case ADD_PHOTOS :
+      return {
+        data: action.photos,
+      };
     default:
       return state;
   }
 };
+
+/* Selectors */
+
+// Get all photos
+export const getPhotos = state => state.photos.data;
+
+// Get photo by cuid
+export const getPhoto = (state, cuid) => state.photos.data.filter(photo => photo.cuid === cuid)[0];
+
 
 export default PhotoReducer;
