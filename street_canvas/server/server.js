a04a5e949e31b217344f18cf6dfb524d136f935f
Corey Wofford <msucorey@gmail.com>
2017-04-06 20:13:54 -0700

    update map pins to link to show pages

--- street_canvas/server/server.js
+++ street_canvas/server/server.js
@@ -95,9 +95,9 @@ const renderFullPage = (html, initialState) => {
         </script>
         <script src='${process.env.NODE_ENV === 'production' ? assetsManifest['/vendor.js'] : '/vendor.js'}'></script>
         <script src='${process.env.NODE_ENV === 'production' ? assetsManifest['/app.js'] : '/app.js'}'></script>
-          <script
-            src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBhceKRzPK-eg1b5nyQ6C1EHLhEkLr5_U4"></script>
 
+        <script
+          src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC3xkNlmWAK_4v52ewKZbnUoifMcYAxIy8"></script>
       </body>
     </html>
   `;
