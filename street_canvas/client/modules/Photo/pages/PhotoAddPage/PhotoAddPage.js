b273970f45fd91652ffbd317ff9340e516d59681
Corey Wofford <msucorey@gmail.com>
2017-04-08 21:26:12 -0700

    add coord capture from Google Maps API

--- street_canvas/client/modules/Photo/pages/PhotoAddPage/PhotoAddPage.js
+++ street_canvas/client/modules/Photo/pages/PhotoAddPage/PhotoAddPage.js
@@ -12,6 +12,11 @@ import { fetchPhoto, addPhotoRequest } from '../../PhotoActions';
 // Import Selectors
 import { getPhoto } from '../../PhotoReducer';
 
+const _getCoordsObj = latLng => ({
+  lat: latLng.lat(),
+  lng: latLng.lng()
+});
+
 
 class PhotoAddPage extends Component {
 
@@ -26,18 +31,34 @@ class PhotoAddPage extends Component {
     };
     this.addPhoto = this.addPhoto.bind(this);
     this.upload = this.upload.bind(this);
+    this.state = { loaded: false };
+    this.initMap = this.initMap.bind(this);
+    this._handleClick = this._handleClick.bind(this);
   }
 
   componentDidMount() {
-    // nothing for now
+    window.initMap = this.initMap;
+    if (!this.state.loaded) {
+      setTimeout(this.initMap, 20);
+    }
+  }
+
+  initMap() {
+    // set the map to show SF
+    const mapOptions = {
+      center: { lat: 37.7758, lng: -122.435 }, // this is SF
+      zoom: 13,
+      disableDefaultUI: true,
+      zoomControl: true,
+    };
+    this.map = new google.maps.Map(this.refs.map, mapOptions);
+    this.setState({ loaded: true });
   }
 
   addPhoto = (e) => {
     e.preventDefault();
     // alert('adding photo from form');
     this.photo.description = this.refs.description.value;
-    this.photo.lat = parseFloat(this.refs.lat.value);
-    this.photo.lng = parseFloat(this.refs.lng.value);
     this.props.addPhoto(
       this.photo.photo_url,
       this.photo.description,
@@ -47,7 +68,7 @@ class PhotoAddPage extends Component {
     );
     setTimeout(() => (
       browserHistory.push(`/photos/${this.props.photos.data[0].cuid}`)
-    ), 20);
+    ), 50);
   };
   //
   // getLocation() {
@@ -70,8 +91,20 @@ class PhotoAddPage extends Component {
     });
   }
 
-  render() {
+  _handleClick(coords) {
+    this.photo.lat = coords.lat;
+    this.photo.lng = coords.lng;
+    alert('coords set');
+  }
 
+  render() {
+    let content = null;
+    if (this.state.loaded) {
+      google.maps.event.addListener(this.map, 'click', event => {
+        const coords = _getCoordsObj(event.latLng);
+        this._handleClick(coords);
+      });
+    }
     return (
       <div >
         <div className={styles['add-container']}>
@@ -83,6 +116,11 @@ class PhotoAddPage extends Component {
             <input placeholder="lng" className={styles['form-field']} ref="lng" /><br />
             <button className={styles['photo-submit-button']} href="#" onClick={this.addPhoto}>Submit</button>
           </form>
+          <div className={styles.map_detail_container}>
+            <p>some text</p>
+            <div className={styles.detailmap} ref="map">Map</div>
+            <p>some more text</p>
+          </div>
         </div>
       </div>
     );
